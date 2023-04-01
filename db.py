e96562e7a80e81f5bcb5270bbec14eeccb50cc77
leAyush98 <rod999lee@gmail.com>
2023-04-01 20:14:17 +0530

    added db class

---
+++ db.py
@@ -0,0 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Movie(db.Model):
+    """ front class members"""
+    id = db.Column(db.Integer, primary_key=True)
+    img_url = db.Column(db.String, unique=True, nullable=False)
+    ranking = db.Column(db.Integer, unique=True, nullable=False)
+
+    """ back class members"""
+    movie_name = db.Column(db.String, unique=True, nullable=False)
+    rating = db.Column(db.String, nullable=False)
+    year = db.Column(db.String, unique=False, nullable=False)
+    review = db.Column(db.String, unique=False, nullable=False)
+    summary = db.Column(db.String, unique=True, nullable=False)
+
+
+    def __init__(self, img_url:str, ranking:int, movie_name:str, rating:str, year:str, review:str, summary:str) -> None:
+           self.img_url = img_url
+           self.ranking = ranking
+           self.movie_name = movie_name
+           self.rating = rating
+           self.year = year
+           self.review = review
+           self.summary = summary
+
--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
+from db import db, Movie
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-Bootstrap(app)
 
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movie-collection.sqlite3'
+db.init_app(app)
+
+Bootstrap(app)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    with app.app_context():
+                db.create_all()   
+
+    first_movie = Movie(img_url="./static/images/drive.png", ranking=10, movie_name="Drive", rating="7.2", year="2011", review="Loved it!", summary="""A mysterious Hollywood
+      stuntman and mechanic moonlights as a getaway driver and finds himself in trouble when he helps out his neighbor in this action drama""")
+    
+    db.session.add(first_movie)
+    db.session.commit()
+
+    return render_template("index.html", first_movie = first_movie)
 
 
 if __name__ == '__main__':
