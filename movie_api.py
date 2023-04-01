da45f5d924424daef2ca6724de744fa891ef353c
leAyush98 <rod999lee@gmail.com>
2023-04-02 04:35:31 +0530

    API class added

--- movie_api.py
+++ movie_api.py
@@ -1,30 +1,24 @@
 import requests
 
-API_KEY = "173589bc6018a70cc128cebac4111119"
-
-IMAGE_URL = "https://image.tmdb.org/t/p/w500"
-
-params_search = {
+class MovieAPI:
+    API_KEY = "173589bc6018a70cc128cebac4111119"
+    IMAGE_URL = "https://image.tmdb.org/t/p/w500"
+    URL = "https://api.themoviedb.org/3"
+    params_search = {
     "api_key" : API_KEY,
     "query" : "captain america"
-}
+    }
 
-params_details = {
-    "api_key" : API_KEY
-}
+    def __init__(self, movie_name) -> None:
+        self.params_search["query"] = movie_name
+
+    def search(self) -> list: 
+        response = requests.get(url=f"{self.URL}/search/movie", params=self.params_search)
+        data = response.json()["results"]
+        return data
 
 
 
-def search():
-    URL = "https://api.themoviedb.org/3/search/movie"
-    response = requests.get(url=URL, params=params_search)
-    data = response.json()["results"]
-    return data
+movie = MovieAPI()
 
-def get_details(id):
-    URL = f"https://api.themoviedb.org/3/movie/{id}"
-    response = requests.get(url=URL , params=params_details)
-    data = response.json()
-    return data
-
-print(get_details(13995))
+print(movie.search())
